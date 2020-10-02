de028fbcd4d89fa01a3073f0d6a137c16868ab4e
timjonez <timjonez@protonmail.com>
2020-10-02 07:35:50 -0500

    [home/views] add test view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
-# Create your views here.
+def home_view(request):
+    return TemplateResponse(request, 'home/home.html', {})
