de04ed5ad2de1065d1dbc38d556ff4e77ee08684
timjonez <timjonez@protonmail.com>
2020-10-03 15:22:06 -0500

    [home/models] add project model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    intro = models.CharField(max_length=200)
+    body = models.TextField()
+    picture = models.ImageField()
+    demo = models.URLField()
+    github = models.URLField()
+    tech = models.JSONField()
+
+    def __str__(self):
+        return self.title
