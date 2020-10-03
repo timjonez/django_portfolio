2593b4ce0500fd695e39b68941ab12fc48fd83d5
timjonez <timjonez@protonmail.com>
2020-10-03 15:23:59 -0500

    [home/urls] update url pattern for projects

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import ProjectView
 
 urlpatterns = [
-    path('', views.home_view),
+    path('', ProjectView.as_view(), name='project_list'),
 ]
