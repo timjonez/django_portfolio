ceae9b99a360cce96d3a0fcb35e0c96bec2e3395
timjonez <timjonez@protonmail.com>
2020-10-04 20:51:09 -0500

    [home/urls] add url pattern for resume

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectView
+from .views import ProjectView, resume_view
 
 urlpatterns = [
     path('', ProjectView.as_view(), name='project_list'),
+    path('resume/', resume_view),
 ]
