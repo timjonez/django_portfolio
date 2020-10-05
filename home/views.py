8c372de2303d83547018a7b5de5d692d946faa09
timjonez <timjonez@protonmail.com>
2020-10-05 09:47:32 -0500

    [home/views] add email and success views

--- home/views.py
+++ home/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
+from django.core.mail import send_mail, BadHeaderError
+
 from .models import Project
+from .forms import ContactForm
 
 
 class ProjectView(ListView):
     model = Project
     template_name = 'home/home.html'
+
+
+def contact_view(request):
+    if request.method == 'GET':
+        form = ContactForm
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            from_email = form.cleaned_data['from_email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['timjonez@protonmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header')
+            return redirect('success/')
+    return render(request, "home/email.html", {'form':form})
+
+
+def success_view(request):
+    return TemplateResponse(request, 'home/success.html', {})
 
 
 def resume_view(request):
