f7cf4ca5c75cff1a6f3d12c85bf624ba8cb0ac0c
timjonez <timjonez@protonmail.com>
2020-10-05 09:48:17 -0500

    [home/urls] add url patterns for contact us and success

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProjectView, resume_view
+from .views import ProjectView, resume_view, contact_view, success_view
 
 urlpatterns = [
     path('', ProjectView.as_view(), name='project_list'),
     path('resume/', resume_view),
+    path('contact/', contact_view),
+    path('contact/success/', success_view),
 ]
