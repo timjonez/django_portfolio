46a502bf94a1ad432c7ff1bf492b52905b024872
timjonez <timjonez@protonmail.com>
2020-10-04 20:50:33 -0500

    [home/views] add view for the resume

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 from django.views.generic.list import ListView
 from .models import Project
 
@@ -6,3 +7,7 @@ from .models import Project
 class ProjectView(ListView):
     model = Project
     template_name = 'home/home.html'
+
+
+def resume_view(request):
+    return TemplateResponse(request, 'home/resume.html', {})
