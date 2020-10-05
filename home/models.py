8b23a06e354ba1950b26b7afcb86986583f7e569
timjonez <timjonez@protonmail.com>
2020-10-05 12:07:38 -0500

    [home/models] allow demo field to be blank

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     intro = models.CharField(max_length=200)
     body = models.TextField()
     picture = models.ImageField()
-    demo = models.URLField()
+    demo = models.URLField(blank=True)
     github = models.URLField()
     tech = models.JSONField()
 
