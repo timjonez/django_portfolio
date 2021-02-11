715aee881b4af1303eb29cec523837b1b7b057af
Tim <timjonez@protonmail.com>
2021-02-11 15:42:26 -0600

    [home/forms] add recaptcha to the email form

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from captcha.fields import ReCaptchaField
 
 
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True, label='Your Email')
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea,required=True)
+    captcha = ReCaptchaField()
